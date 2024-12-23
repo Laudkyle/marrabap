a6bb3fbd116ce4ab3c65e69d6040fd4d61cd3dea
Laudkyle <kyleaby1@gmail.com>
2024-12-23 13:54:54 +0000

    A few changes

--- src/components/EditProduct.jsx
+++ src/components/EditProduct.jsx
@@ -187,7 +187,7 @@ const EditProduct = ({ onProductUpdated }) => {
               value={formData.stock}
               onChange={handleChange}
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-              required
+              readOnly
             />
           </div>
 
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -515,7 +515,7 @@ const ProductList = () => {
                     value={selectedProduct?.stock}
                     onChange={handleChange}
                     className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    required
+                    readOnly
                   />
                 </div>
               </div>
